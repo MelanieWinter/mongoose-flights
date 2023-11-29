ce66e5f502cd559c6ca4a7b4ae4fe7bce516e301
Melanie Winter <melaniebucher830@gmail.com>
2023-11-28 22:04:39 -0800

    Creates Flight model

--- models/flight.js
+++ models/flight.js
@@ -2,13 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date
+    airline: { 
+        type: String, 
+        enum: [ 'American', 'Southwest', 'United', 'Delta', 'Alaska', 'Spirit', 'Hawaiian' , 'JetBlue', 'Frontier', 'Allegiant']
+    },
+    airport: { 
+        type: String, 
+        default: 'DEN', 
+        enum: [ 'ATL', 'LAX', 'ORD', 'DFW', 'DEN', 'JFK', 'SFO', 'SEA', 'LAS', 'CLT']
+    },
+    flightNo: { 
+        type: Number, 
+        required: true, 
+        min: 10, 
+        max: 9999 
+    },
+    departs: { 
+        type: Date, 
+        default: function() {
+        const nextYear = new Date()
+        nextYear.setFullYear(nextYear.getFullYear() + 1)
+        return nextYear
+    }}
 }, {
     timestamps: true
 });
 
 // Compile the schema into a model and export it
-module.exports = mongoose.model('Movie', movieSchema);
+module.exports = mongoose.model('Flight', flightSchema);
