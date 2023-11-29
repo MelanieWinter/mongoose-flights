2333faa039ca48eb06fb31e363dea8657e9c9ea1
Melanie Winter <melaniebucher830@gmail.com>
2023-11-28 21:27:19 -0800

    Adds initial code for mongoose-flights deliverable. Includes .env, partials, database, and model files.

---
+++ models/flight.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: String,
+    airport: String,
+    flightNo: Number,
+    departs: Date
+}, {
+    timestamps: true
+});
+
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Movie', movieSchema);
